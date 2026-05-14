ec926c4e51a4192e6ca3308b69634d07740ba29f
Bonn-007 <bgwaabe1121@gmail.com>
2026-05-14 18:23:36 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://0ov4kxrnkb.execute-api.us-east-2.amazonaws.com/default/lamda-1'
+const API_ENDPOINT = 'https://unuk3b0hr2.execute-api.us-east-2.amazonaws.com/default/lamda-1'
 
 export default {
   name: 'app',
